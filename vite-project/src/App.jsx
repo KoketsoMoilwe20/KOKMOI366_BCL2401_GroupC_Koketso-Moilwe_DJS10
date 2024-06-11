867a4a91a0b830cf7a46ebb4a66f5f8934f970d1
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-11 14:07:38 +0200

    fetch API

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './App.css'
 
 //Fetching DATA from an API
+const fetchAPI = fetch("https://jsonplaceholder.typicode.com/posts")
 
+fetchAPI.then(response => response.json())
+  .then(posts => {console.log(posts);
+    
+})
 
 function App() {
 
