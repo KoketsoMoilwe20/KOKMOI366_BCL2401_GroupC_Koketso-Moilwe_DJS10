63f1a4823c9767e1f2d29e8ac4a8dfb832a6eeee
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-14 15:11:54 +0200

    set error state

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [data, setData ] = useState();
+  const [error, setError] = useState();
   
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => setData(data))
+
   })
 
 }
