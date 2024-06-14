cab03d0bfbd28b62dde3d72b4d45042947929199
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-14 15:10:06 +0200

    set data

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [data, setData ] = useState();
+  
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-    
+      .then((data) => setData(data))
   })
 
 }
