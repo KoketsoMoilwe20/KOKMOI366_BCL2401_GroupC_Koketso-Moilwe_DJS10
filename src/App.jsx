c32efd298281da08beef2751a6929a99d5a93103
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-14 16:15:24 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+
 function App() {
-  const [data, setData ] = useState();
+  //logic
   const [error, setError] = useState();
-  
+  const [data, setData] = useState();
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
-      .then((data) => setData(data))
+      .then((posts) => setData(posts))
       .catch((error) => {
         setError(error);
       });
-  }, [])
+  }, []);
+  // setError("Now there's an error")
 
-    return (
-      <>
-        {!error && data
+  return (
+    <>
+    <h1>Posts</h1>
+      {!error && data
         ? data.map((post) => (
-            <div key={userId}> 
+            <div> 
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
           ))
         : null}
       {error ? <h1>{error}</h1> : null}
-      </>
-    );
-
+    </>
+  );
 }
 
-export default App
+export default App;
