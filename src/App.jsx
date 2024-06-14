58dbc0d10f37c64d526384db9f1f84bd65f4c733
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-06-14 15:06:48 +0200

    use a callback function using useEffect hook
    to fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { useState, useEffect } from "react";
 function App() {
   const [data, setData ] = useState();
 
-
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    
+  })
 
 }
